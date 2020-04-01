1cadd477e9db550e3c43018b78953573c1251a56
kevin <pier_rv@hotmail.es>
2020-04-01 13:14:24 -0500

    my-frist-app.v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Employee } from './models/employee'
+import { emptyScheduled } from 'rxjs/internal/observable/empty';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My-frist-app';
+  employeeArray:Employee[]=[
+    {id:1,name:'kevin',country:'peru'},
+    {id:2,name:'jose',country:'bolivia'}
+  ];
+
+  selectedEmployee:Employee = new Employee();
+
+  openEdit(employee:Employee){
+    this.selectedEmployee = employee;
+  }
+
+  addOedit(){
+    if(this.selectedEmployee.id === 0 ){
+      this.selectedEmployee.id = this.employeeArray.length+1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+    this.selectedEmployee = new Employee();
+  }
+
+  deleteItem(){
+    if( confirm ('quieres eliminar!')){
+      this.employeeArray = this.employeeArray.filter(x => x !=this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+  }
 }
